e4d3ef001468b0c8e66d5f5fa83e02a473907d2a
Mendelt Siebenga <msiebenga@gmail.com>
2019-03-03 16:35:19 +0100

    Update dependencies

--- src/manifest.rs
+++ src/manifest.rs
@@ -73,7 +73,7 @@ mod tests {
         Manifest {
             base_path: PathBuf::new(),
             values: BTreeMap::new(),
-            templates: templates,
+            templates,
         }
     }
 
